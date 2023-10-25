6a52655be435e553e1f5d78957fc8e743da4a208
Spirit <66965600+codedotspirit@users.noreply.github.com>
2023-10-25 15:51:24 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+});
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 }
 
-module.exports = nextConfig
+module.exports = withNextra(nextConfig);
---
+++ theme.config.tsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>SPARK Documentation</span>,
+  project: {
+    link: 'https://github.com/spark-cms/spark-docs'
+  }
+  // ... other theme options
+}
