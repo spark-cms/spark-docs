eea338a52df28664f376610f33d398048fd6ab5f
Spirit <louisescher@proton.me>
2023-10-26 20:21:00 +0200

    Update looks, SEO, icons and add first pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,68 @@
+import { usePathname } from "next/navigation"
+
 export default {
-  logo: <span>SPARK Documentation</span>,
   project: {
     link: 'https://github.com/spark-cms/spark-docs'
+  },
+  useNextSeoProps() {
+    const pathname = usePathname();
+
+    if (pathname !== '/') {
+      return {
+        titleTemplate: '%s - SPARK'
+      }
+    } else {
+      return {
+        titleTemplate: 'Documentation - SPARK'
+      }
+    }
+  },
+  darkMode: true,
+  primaryHue: 23,
+  primarySaturation: 100,
+  logo: (
+    <div style={{ display: "flex", alignItems: "center", fontSize: "1.2rem", fontWeight: "900", color: "#fff" }}>
+      <img src="/img/spark_logo.svg" alt="Logo" style={{ marginRight: "0.5rem", width: "3rem" }} />
+      <span>SPARK</span>
+    </div>
+  ),
+  logoLink: "/",
+  banner: {
+    dismissible: true,
+    key: "spark-alpha-1",
+    text: "⚠️ SPARK is in alpha. Expect bugs and breaking changes. ⚠️",
+  },
+  feedback: {
+    content: null
+  },
+  footer: {
+    text: (
+      <span>
+        {new Date().getFullYear()} ©{' '}
+        <a href="https://entropic.software" target="_blank">
+          Entropic Software
+        </a>
+        . All rights reserved.
+      </span>
+    )
+  },
+  editLink: {
+    text: "Edit this page on GitHub",
+    component: ({ className, children, filePath }: { className?: string, filePath?: string, children: React.ReactNode }) => (
+      <a
+        href={`https://github.com/spark-cms/spark-docs/blob/master/${filePath}`}
+        target="_blank"
+        className={className}
+      >
+        {children}
+      </a>
+    )
+  },
+  toc: {
+    backToTop: true,
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1
   }
   // ... other theme options
 }
