ffe71c32535628e2731e3d5cc81c82741e049059
Spirit <louisescher@proton.me>
2023-10-31 21:27:49 +0100

    Adjust SEO

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,35 @@
-import { usePathname } from "next/navigation"
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+import { useConfig } from 'nextra-theme-docs';
 
 export default {
   project: {
     link: 'https://github.com/spark-cms/spark-docs'
+  },
+  head: () => {
+    const { asPath } = useRouter();
+    const config = useConfig();
+
+    const title = asPath === "/" ? "Documentation - SPARK" : config.title + " - SPARK";
+
+    return (
+      <>
+        <meta property="og:url" content={`https://docs.spark-cms.io${asPath}`} />
+        <meta name="description" content="This is the official documentation for SPARK." />
+        <meta property="og:description" content="This is the official documentation for SPARK." />
+        <meta property="theme-color" content="#ff7b28" />
+        <meta name="apple-mobile-web-app-title" content="SPARK Docs" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@EntropicSoft" />
+        <meta name="twitter:creator" content="@EntropicSoft" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content="This is the official documentation for SPARK." />
+        <meta name="twitter:image" content="https://docs.spark-cms.io/img/SPARK_og_banner.png" />
+        <meta name="twitter:image:width" content="2400" />
+        <meta name="twitter:image:height" content="1256" />
+        <meta name="twitter:image:alt" content="SPARK Logo on a dark background" />
+      </>
+    )
   },
   useNextSeoProps() {
     const pathname = usePathname();
@@ -63,6 +90,7 @@ export default {
   },
   sidebar: {
     defaultMenuCollapseLevel: 1
-  }
+  },
+  docsRepositoryBase: "https://github.com/spark-cms/spark-docs"
   // ... other theme options
 }
